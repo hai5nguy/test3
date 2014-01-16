572206347cb22c4028387c52c1c4c193843938cb
nmhai3 <nmhai3@gmail.com>
2014-01-16 10:51:07 -0500

    initial commit

---
+++ test3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Name = "blah1 |  blah2 | blah3|     ||";
+
+            string[] Names = Name.Split('|').Select(n => n.Trim()).ToArray();
+
+            
+        }
+    }
+}
